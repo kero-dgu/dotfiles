98b64afe98691a0c9aee02f4ecb8d7c66f6f6556
kero-dgu <kero.dgu@gmail.com>
2015-01-26 23:30:17 +0900

    Updated .vimrc

---
+++ dotfiles_link.bat
@@ -0,0 +1,6 @@
+mklink %HOME%\.bashrc %HOME%\dotfiles\.bashrc
+mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
+mklink %HOME%\.vim %HOME%\dotfiles\.vim
+mklink %HOME%\.vim\backup %HOME%\dotfiles\.vim\backup
+mklink %HOME%\.vim\bundle %HOME%\dotfiles\.vim\bundle
+mklink %HOME%\.vim\colors %HOME%\dotfiles\.vim\colors
